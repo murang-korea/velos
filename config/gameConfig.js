fa5c613836f5753d2970fae209284778a7a706aa
woojik <woojik1220@gmail.com>
2026-02-10 12:32:34 +0900

    Create gameConfig.js

---
+++ config/gameConfig.js
@@ -0,0 +1,18 @@
+export const GAME_CONFIG = {
+    SCREEN: {
+        REFERENCE_WIDTH: 1920,
+        REFERENCE_HEIGHT: 1080
+    },
+    PLAYER: {
+        MAX_HP: 100,
+        DEFAULT_SPEED: 200, // px per second
+        SIZE: 50
+    }
+};
+
+export const STATES = {
+    IDLE: 'IDLE',
+    MOVE: 'MOVE',
+    ATTACK: 'ATTACK',
+    DEAD: 'DEAD'
+};
